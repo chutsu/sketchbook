aae6d4db210efa18f6b32505793d4022da4c08bd
Chris Choi <chutsu@gmail.com>
2024-06-16 15:44:56 +0100

    Update

--- ml/linear_discriminant_analysis.py
+++ ml/linear_discriminant_analysis.py
@@ -6,30 +6,20 @@ from numpy.linalg import inv
 from numpy.linalg import det
 import matplotlib.pyplot as plt
 
-# Generate two groups of data with different means by same covariance
-mu1 = [0, 0]
-mu2 = [1, 2]
-cov = [[1, 0], [0, 1]]
-n1 = 50
-n2 = 50
-n = n1 + n2
-x1 = np.random.multivariate_normal(mean=mu1, cov=cov, size=n1)
-x2 = np.random.multivariate_normal(mean=mu2, cov=cov, size=n2)
-x = np.vstack([x1, x2])
-y = np.concatenate([np.repeat(1, n1), np.repeat(2, n2)])
 
-# Estimate priors
-pi_hat_1 = n1 / n
-pi_hat_2 = n2 / n
-
-# Estimate means
-mu_hat_1 = 1 / n1 * np.sum(x1, axis=0)
-mu_hat_2 = 1 / n2 * np.sum(x2, axis=0)
-
-# Estimate sample covariance
-cov_hat_1 = 1 / (n1 - 1) * ((x1 - mu_hat_1).T @ (x1 - mu_hat_1))
-cov_hat_2 = 1 / (n2 - 1) * ((x2 - mu_hat_2).T @ (x2 - mu_hat_2))
-cov_hat = (cov_hat_1 + cov_hat_2) / 2
+def generate_data():
+  """Generate two groups of data with different means by same covariance"""
+  mu1 = [0, 0]
+  mu2 = [1, 2]
+  cov = [[1, 0], [0, 1]]
+  n1 = 50
+  n2 = 50
+  n = n1 + n2
+  x1 = np.random.multivariate_normal(mean=mu1, cov=cov, size=n1)
+  x2 = np.random.multivariate_normal(mean=mu2, cov=cov, size=n2)
+  x = np.vstack([x1, x2])
+  y = np.concatenate([np.repeat(1, n1), np.repeat(2, n2)])
+  return x1, x2, x, y, n1, n2, n
 
 
 def log_likelihood(x, mu, sigma, prior):
@@ -48,38 +38,46 @@ def abline(slope, intercept):
   plt.plot(x_vals, y_vals, '--')
 
 
-point_grid = np.mgrid[-10:10.1:0.5, -10:10.1:0.5].reshape(2, -1).T
-ll_vals_1 = [log_likelihood(x, mu_hat_1, cov_hat, pi_hat_1) for x in point_grid]
-ll_vals_2 = [log_likelihood(x, mu_hat_2, cov_hat, pi_hat_2) for x in point_grid]
+if __name__ == "__main__":
+  # Generate Data
+  x1, x2, x, y, n1, n2, n = generate_data()
 
-# plt.figure()
-# plt.scatter(x[:, 0], x[:, 1], c=y)
+  # Estimate priors
+  pi_hat_1 = n1 / n
+  pi_hat_2 = n2 / n
 
-# plt.figure()
-# plt.scatter(point_grid[:, 0], point_grid[:, 1], c=ll_vals_1)
-# plt.plot(mu_hat_1[0], mu_hat_1[1], 'rp', markersize=14)
-# plt.colorbar()
+  # Estimate means
+  mu_hat_1 = 1 / n1 * np.sum(x1, axis=0)
+  mu_hat_2 = 1 / n2 * np.sum(x2, axis=0)
 
-# plt.figure()
-# plt.scatter(point_grid[:, 0], point_grid[:, 1], c=ll_vals_2)
-# plt.plot(mu_hat_2[0], mu_hat_2[1], 'rp', markersize=14)
-# plt.colorbar()
+  # Estimate sample covariance
+  cov_hat_1 = 1 / (n1 - 1) * ((x1 - mu_hat_1).T @ (x1 - mu_hat_1))
+  cov_hat_2 = 1 / (n2 - 1) * ((x2 - mu_hat_2).T @ (x2 - mu_hat_2))
+  cov_hat = (cov_hat_1 + cov_hat_2) / 2
 
-# Slope
-cov_inv = inv(cov_hat)
-slope_vec = cov_inv @ (mu_hat_1 - mu_hat_2)
-slope = -slope_vec[0] / slope_vec[1]
+  # Calculate log-liklihood
+  grid = np.mgrid[-10:10.1:0.5, -10:10.1:0.5].reshape(2, -1).T
+  ll_vals_1 = [log_likelihood(x, mu_hat_1, cov_hat, pi_hat_1) for x in grid]
+  ll_vals_2 = [log_likelihood(x, mu_hat_2, cov_hat, pi_hat_2) for x in grid]
 
-# Intercept
-intercept_partial = log(pi_hat_2 / pi_hat_1) + 0.5 * (mu_hat_1.T @ cov_inv @ mu_hat_1) - 0.5 * (mu_hat_2.T @ cov_inv @ mu_hat_2)
-intercept = intercept_partial / slope_vec[1]
+  # Slope
+  cov_inv = inv(cov_hat)
+  slope_vec = cov_inv @ (mu_hat_1 - mu_hat_2)
+  slope = -slope_vec[0] / slope_vec[1]
 
-# plotting
-plt.figure()
-plt.scatter(x[:, 0], x[:, 1], c=y)
-abline(slope, intercept)
-plt.plot(mu_hat_1[0], mu_hat_1[1], 'rp', markersize=14)
-plt.plot(mu_hat_2[0], mu_hat_2[1], 'rp', markersize=14)
-plt.axis("equal")
+  # Intercept
+  intercept_partial = log(pi_hat_2 / pi_hat_1) + 0.5 * (
+      mu_hat_1.T @ cov_inv @ mu_hat_1) - 0.5 * (mu_hat_2.T @ cov_inv @ mu_hat_2)
+  intercept = intercept_partial / slope_vec[1]
 
-plt.show()
+  # plotting
+  plt.figure()
+  plt.scatter(x[:, 0], x[:, 1], c=y)
+  abline(slope, intercept)
+  plt.plot(mu_hat_1[0], mu_hat_1[1], 'rp', markersize=14)
+  plt.plot(mu_hat_2[0], mu_hat_2[1], 'rp', markersize=14)
+  plt.xlabel("x")
+  plt.ylabel("y")
+  plt.axis("equal")
+
+  plt.show()
--- ml/logistic_regression.py
+++ ml/logistic_regression.py
@@ -1,28 +1,4 @@
 #!/usr/bin/env python3
-"""
-Logistic Regression
-===================
-
-Logistic regression is a statistical method used for modeling the probability
-of a **binary outcome** (i.e., a dependent variable with two possible outcomes,
-such as success/failure, yes/no, or 0/1) based on one or more predictor
-variables (independent variables). It is a type of regression analysis used
-when the dependent variable is categorical.
-
-The core of logistic regression is the logit function, which is the natural
-logarithm of the odds of the dependent event occurring.
-
-The logit function is expressed as:
-
-  logit(p) = log(p / 1 - p)
-
-where p is the probability of the event occurring.
-
-The logistic regression model is formulated as:
-
-  logit(p) = beta_0 + beta_1 * x_1 + beta_2 * x_2 + \dots + beta_k * x_k
-
-"""
 import numpy as np
 from numpy import exp
 from numpy import log
