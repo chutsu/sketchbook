e3c30c86c59cbd71c5efdbce47d33afe6441c6f8
Chris Choi <chutsu@gmail.com>
2026-04-12 16:51:17 +0100

    Add note-sparse_matrix.py

---
+++ leet/note-sparse_matrix.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+B = np.array([[1, 2], [4, 5], [7, 8]])
+
+res = np.zeros((3, 2))
+
+for i in range(A.shape[0]):
+  for j in range(A.shape[1]):
+    for k in range(B.shape[1]):
+      res[i, k] += A[i, j] * B[j, k]
+
+print(res)
+print(A @ B)
