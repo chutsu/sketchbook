e773303d9e6bc8154fa18887fa325977daac31ab
Chris Choi <chutsu@gmail.com>
2026-04-12 16:53:36 +0100

    Update

--- leet/problem-dot_product_of_two_sparse_vectors.py
+++ leet/problem-dot_product_of_two_sparse_vectors.py
@@ -7,7 +7,8 @@ Given two sparse vectors, compute their dot product.
 Implement class SparseVector:
 
     SparseVector(nums) Initializes the object with the vector nums
-    dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+    dotProduct(vec) Compute the dot product between the instance of
+    SparseVector and vec
 
 A sparse vector is a vector that has mostly zero values, you should store the
 sparse vector efficiently and compute the dot product between two SparseVector.
