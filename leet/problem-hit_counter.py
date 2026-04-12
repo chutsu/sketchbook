ab092863b73efca8403cc192dc4116f44379f83c
Chris Choi <chutsu@gmail.com>
2026-04-12 16:51:40 +0100

    Add problem-hit_counter.py

---
+++ leet/problem-hit_counter.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+Design a hit counter which counts the number of hits received in the past 5
+minutes (i.e., the past 300 seconds).
+
+Your system should accept a timestamp parameter (in seconds granularity), and
+you may assume that calls are being made to the system in chronological order
+(i.e., timestamp is monotonically increasing). Several hits may arrive roughly
+at the same time.
+
+Implement the HitCounter class:
+
+    HitCounter()
+      Initializes the object of the hit counter system.
+
+    void hit(int timestamp)
+      Records a hit that happened at timestamp (in seconds). Several hits may
+      happen at the same timestamp.
+
+    int getHits(int timestamp)
+      Returns the number of hits in the past 5 minutes
+      from timestamp (i.e., the past 300 seconds).
+
+
+
+Example 1:
+
+Input
+  ["HitCounter", "hit", "hit", "hit", "getHits", "hit", "getHits", "getHits"]
+  [[], [1], [2], [3], [4], [300], [300], [301]]
+
+Output
+  [null, null, null, null, 3, null, 4, 3]
+
+Explanation
+
+HitCounter hitCounter = new HitCounter();
+hitCounter.hit(1);       // hit at timestamp 1.
+hitCounter.hit(2);       // hit at timestamp 2.
+hitCounter.hit(3);       // hit at timestamp 3.
+hitCounter.getHits(4);   // get hits at timestamp 4, return 3.
+hitCounter.hit(300);     // hit at timestamp 300.
+hitCounter.getHits(300); // get hits at timestamp 300, return 4.
+hitCounter.getHits(301); // get hits at timestamp 301, return 3.
+
+Constraints:
+
+    1 <= timestamp <= 2 * 109
+    All the calls are being made to the system in chronological order (i.e.,
+    timestamp is monotonically increasing).
+    At most 300 calls will be made to hit and getHits.
+
+"""
+
+
+class HitCounter:
+  def __init__(self):
+    self.q = []
+
+  def hit(self, timestamp: int) -> None:
+    self.q.append(timestamp)
+
+  def getHits(self, timestamp: int) -> int:
+    while self.q and self.q[0] <= timestamp - 300:
+      self.q.pop(0)
+    return len(self.q)
+
+
+class HitCounter2:
+  def __init__(self):
+    self.times = [0] * 300
+    self.hits = [0] * 300
+
+  def hit(self, timestamp: int) -> None:
+    idx = timestamp % 300
+
+    if self.times[idx] != timestamp:
+      self.times[idx] = timestamp
+      self.hits[idx] = 1
+    else:
+      self.hits[idx] += 1
+
+  def getHits(self, timestamp: int) -> int:
+    total = 0
+    for i in range(300):
+      if timestamp - self.times[i] < 300:
+        total += self.hits[i]
+    return total
+
+
+if __name__ == "__main__":
+  counter = HitCounter()
+
+  counter.hit(1)  # hit at timestamp 1.
+  counter.hit(2)  # hit at timestamp 2.
+  counter.hit(3)  # hit at timestamp 3.
+  assert counter.getHits(4) == 3  # get hits at timestamp 4, return 3.
+
+  counter.hit(300)  # hit at timestamp 300.
+  assert counter.getHits(300) == 4  # get hits at timestamp 300, return 4.
+  assert counter.getHits(301) == 3  # get hits at timestamp 301, return 3.
+
+
+  # Hit counter 2
+  counter2 = HitCounter2()
+
+  counter2.hit(1)  # hit at timestamp 1.
+  counter2.hit(2)  # hit at timestamp 2.
+  counter2.hit(3)  # hit at timestamp 3.
+  assert counter2.getHits(4) == 3  # get hits at timestamp 4, return 3.
+
+  counter2.hit(300)  # hit at timestamp 300.
+  assert counter2.getHits(300) == 4  # get hits at timestamp 300, return 4.
+  assert counter2.getHits(301) == 3  # get hits at timestamp 301, return 3.
